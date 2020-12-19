23228a39cadb16a9d1e0284687f64bf6dcb3d38c
Athena Hernandez <athena.hernandez.23@Bshops.com>
2020-12-18 20:49:26 -0800

    Got the eat function to work

--- code.js
+++ code.js
@@ -1,72 +1,115 @@
+class Circle {
+	// Notice that the constructor takes some of its values as inputs,
+	//   and sets others by itself.
+	constructor(x, y, radius) {
+		this.circlePos = [x, y, radius];
+		this.circleVel = [0, 0, 0];
+	}
 
+  draw() {
+		/*
+	    Parameters: None
+	    Returns: None
+	    Purpose: To draw the user's circle in its new position.
+	  */
+		context.beginPath();
+		context.arc(this.circlePos[0], this.circlePos[1], this.circlePos[2], 0, 2 * Math.PI);
+		context.fillStyle = "blue";
+		context.strokeStyle = "blue";
+		context.fill();
+		context.stroke();
+  }
 
-// class Circle {
-// 	// Notice that the constructor takes some of its values as inputs,
-// 	//   and sets others by itself.
-// 	constructor(x, y, r, sAngle, eAngle) {
-// 		this.x = x;
-// 		this.y = y;
-// 		this.center = [x, y];
-// 		this.color = "#0000ff";
-// 	}
-//
-//
-//   draw() {
-//     context.strokeStyle = this.color;
-//     context.x = this.x;
-//     context.y = this.y;
-//     context.beginPath();
-//     context.arc(100, 75, 50, 0, 2 * Math.PI, context);
-//     context.stroke();
-//   }
-// }
-//
-// function setUpContext() {
-//   // Get width/height of the browser window
-//   console.log("Window is %d by %d", window.innerWidth, window.innerHeight);
-//
-//   // Get the canvas, set the width and height from the window
-//   canvas = document.getElementById("mainCanvas");
-//   // I found that - 20 worked well for me, YMMV
-//   canvas.width = window.innerWidth - 20;
-//   canvas.height = window.innerHeight - 20;
-//   canvas.style.border = "1px solid black";
-//
-//   // Set up the context for the animation
-//   context = canvas.getContext("2d");
-//   return context;
-// }
-//
-//
-// 	function myKeyDown(event) {
-// 	  /*
-// 	    Parameters: event object, which contains information about the event
-// 	      that triggered the event listener.
-// 	    Returns: None, but modifies global variables which track response to event.
-// 	    Purpose: Make the animation respond to keys being pressed.
-// 	  */
-// 	  // One of the attributes of the event object is 'which,' contains the key
-// 	  //   that was pressed to trigger the event listener.
-// 	  keyCode = event.which;
-// 	  keyStr = event.key;
-// 	  console.log(event);
-// 	  console.log(keyCode);
-// 	  console.log(keyStr);
-//
-// 	  if (keyStr == 'w') {
-// 	    // Move circle up
-// 	    circleVel[1] -= 1;
-// 	  }
-// 	  if (keyStr == 'a') {
-// 	    // Move circle left
-// 	    circleVel[0] -= 1;
-// 	  }
-// 	  if (keyStr == 's') {
-// 	    // Move circle down
-// 	    circleVel[1] += 1;
-// 	  }
-// 	  if (keyStr == 'd') {
-// 	    // Move circle right
-// 	    circleVel[0] += 1;
-// 	  }
-// 	}
+	applyVelocity() {
+		/*
+		    Parameters: Position and Velocity of an object
+		      Assumption: Velocity list has same length as position list.
+		    Returns: None, but modifies global variables
+		    Purpose: Apply velocity to position, moving the object.
+		  */
+		var i = 0;
+		for (i = 0; i < this.circlePos.length; i++) {
+			this.circlePos[i] += this.circleVel[i];
+		}
+	}
+
+	myKeyDown(event) {
+		/*
+		    Parameters: event object, which contains information about the event
+		      that triggered the event listener.
+		    Returns: None, but modifies global variables which track response to event.
+		    Purpose: Make the animation respond to keys being pressed.
+		  */
+		// One of the attributes of the event object is 'which,' contains the key
+		//   that was pressed to trigger the event listener.
+		keyCode = event.which;
+		keyStr = event.key;
+		console.log(event);
+		console.log(keyCode);
+		console.log(keyStr);
+		if (keyStr == 'w') {
+			// Move circle up
+			this.circleVel[1] -= 1;
+		}
+		if (keyStr == 'a') {
+			// Move circle left
+			this.circleVel[0] -= 1;
+		}
+		if (keyStr == 's') {
+			// Move circle down
+			this.circleVel[1] += 1;
+		}
+		if (keyStr == 'd') {
+			// Move circle right
+			this.circleVel[0] += 1;
+		}
+	}
+}
+
+function checkFoodEaten() {
+	/*
+		Parameters: None
+		Returns: None, but modifies global variables
+		Purpose: Checks whether the user's circle is overlapping with any food
+	*/
+	for (i = 0; i < food.length; i++) {
+		var xDistance = Math.abs(food[i][0] - circle.circlePos[0]);
+		var yDistance = Math.abs(food[i][1] - circle.circlePos[1]);
+		var distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+		var sumOfRadii = 7 + circlePos[2];
+
+		if (distance < sumOfRadii) {
+			food.splice(i, 1);
+		}
+	}
+}
+
+function addFood() {
+	/*
+			Parameters: None
+			Returns: None, but modifies global variables
+			Purpose: Creates the initial list of food
+		*/
+	for (i = 0; i < canvas.width / 20; i++) {
+			randomX = Math.floor(Math.random() * canvas.width);
+			randomY = Math.floor(Math.random() * canvas.height);
+			randomColorIndex = Math.floor(Math.random() * (foodColors.length+1));
+			food.push([randomX, randomY, foodColors[randomColorIndex]]);
+	}
+}
+
+function setUpContext() {
+  // Get width/height of the browser window
+  console.log("Window is %d by %d", window.innerWidth, window.innerHeight);
+
+  // Get the canvas, set the width and height from the window
+  canvas = document.getElementById("mainCanvas");
+  // I found that - 20 worked well for me, YMMV
+  canvas.width = window.innerWidth - 20;
+  canvas.height = window.innerHeight - 20;
+  canvas.style.border = "1px solid black";
+
+  // Set up the context for the animation
+  context = canvas.getContext("2d");
+  return context;
+}
--- structure.js
+++ structure.js
@@ -2,19 +2,8 @@
 var circlePos = [0, 0, 25];
 var circleVel = [0, 0, 0];
 var food = [];
-
-function applyVelocity(position, velocity) {
-	/*
-	    Parameters: Position and Velocity of an object
-	      Assumption: Velocity list has same length as position list.
-	    Returns: None, but modifies global variables
-	    Purpose: Apply velocity to position, moving the object.
-	  */
-	var i = 0;
-	for (i = 0; i < position.length; i++) {
-		position[i] += velocity[i];
-	}
-}
+var foodColors = ["#bc09ff", "#1e00ff", "#ff5f07", "#09fff6", "#fe09ad",
+									"#ff061d", "#01c9ff", "#74ff09"];
 
 function myKeyDown(event) {
 	/*
@@ -32,30 +21,23 @@ function myKeyDown(event) {
 	console.log(keyStr);
 	if (keyStr == 'w') {
 		// Move circle up
-		circleVel[1] -= 1;
+		circle.circleVel[1] -= 1;
 	}
 	if (keyStr == 'a') {
 		// Move circle left
-		circleVel[0] -= 1;
+		circle.circleVel[0] -= 1;
 	}
 	if (keyStr == 's') {
 		// Move circle down
-		circleVel[1] += 1;
+		circle.circleVel[1] += 1;
 	}
 	if (keyStr == 'd') {
 		// Move circle right
-		circleVel[0] += 1;
+		circle.circleVel[0] += 1;
 	}
 }
 
-function addFood() {
-	for (i = 0; i < canvas.width / 20; i++) {
-			randomX = Math.floor(Math.random() * canvas.width);
-			randomY = Math.floor(Math.random() * canvas.height);
-			food.push([randomX, randomY]);
-	}
-}
-
+var frames = 0;
 function drawAll()
 /*
   Purpose: This is the main drawing loop.
@@ -63,46 +45,45 @@ function drawAll()
   Returns: None, but it calls itself to cycle to the next frame
 */
 {
-	applyVelocity(circlePos, circleVel);
+	frames++;
+	circle.applyVelocity();
 
+	// Adds a piece of food every 90 frames
+	if (frames % 90 == 0) {
+		randomX = Math.floor(Math.random() * canvas.width);
+		randomY = Math.floor(Math.random() * canvas.height);
+		randomColorIndex = Math.floor(Math.random() * (foodColors.length+1));
+		food.push([randomX, randomY, foodColors[randomColorIndex]]);
+	}
+
+	checkFoodEaten();
 	// Draw the circle
 	context.clearRect(0, 0, canvas.width, canvas.height);
-	for (i = 0; i < food.length; i++) {
+
+	// Redraw food
+	for (var i = 0; i < food.length; i++) {
 		context.beginPath();
-		console.log(food[i][0], food[i][1])
+		//console.log(food[i][0], food[i][1]);
 		context.arc(food[i][0], food[i][1], 7, 0, 2 * Math.PI);
+		context.fillStyle = food[i][2];
+		context.strokeStyle = food[i][2];
+		context.fill();
 		context.stroke();
 	}
-	context.beginPath();
-	context.arc(circlePos[0], circlePos[1], circlePos[2], 0, 2 * Math.PI);
-	context.stroke();
-	// Loop the animation to the next frame.
+
+	circle.draw();
 	window.requestAnimationFrame(drawAll);
 }
 
-
 // Get width/height of the browser window
-windowWidth = window.innerWidth;
-windowHeight = window.innerHeight;
-console.log("Window is %d by %d", windowWidth, windowHeight);
-
-// Get the canvas, set the width and height from the window
-canvas = document.getElementById("mainCanvas");
-
-// I found that - 20 worked well for me, YMMV
-canvas.width = windowWidth - 20;
-canvas.height = windowHeight - 20;
-canvas.style.border = "1px solid black";
-
-// Set up the context for the animation
-context = canvas.getContext("2d");
+context = setUpContext();
 
 // Set up event listener for when user presses a key down.
 // It then calls the function myKeyDown, passing it an event object.
 document.addEventListener("keydown", myKeyDown);
 
 // Start the circle in the center of the canvas.
-circlePos = [Math.floor(canvas.width / 2), Math.floor(canvas.height / 2), 25];
+circle = new Circle(Math.floor(canvas.width / 2), Math.floor(canvas.height / 2), 25, context);
 addFood();
 
 // Fire up the animation engine
